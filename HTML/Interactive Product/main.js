957616ad54f7ab05330bfc52ce0e2c441ab48cd2
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2022-11-22 18:00:41 +0700

    update iphone XR

--- HTML/Interactive Product/main.js
+++ HTML/Interactive Product/main.js
@@ -1,10 +1,27 @@
 window.onload = () => {
   const capacity_btn = document.querySelectorAll('.capacity .size');
+  const colour_btn = document.querySelectorAll('.colours .colour');
+  const image_el = document.querySelector('.imagery .image');
+  const imagery_el = document.querySelector('.imagery');
+
   for(let i = 0; i < capacity_btn.length; i++) {
     let btn = capacity_btn[i];
     btn.addEventListener('click', function() {
       document.querySelector('.capacity .size.selected').classList.remove('selected');
       this.classList.add('selected');
+    });
+  }
+
+  
+
+  for(let i = 0; i < colour_btn.length; i++) {
+    let btn = colour_btn[i];
+
+    btn.addEventListener('click', function() {
+      document.querySelector('.colours .colour.selected').classList.remove('selected');
+      this.classList.add('selected');
+      image_el.src = "images/xr-" + this.dataset.name + '.png';
+      imagery_el.style.backgroundColor = this.dataset.colour;
     })
   }
 }
