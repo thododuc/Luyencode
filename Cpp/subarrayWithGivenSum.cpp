463aa8697a1768f36db72cb6728260ce02e37c4e
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2022-12-06 21:40:19 +0700

    angular12

---
+++ Cpp/subarrayWithGivenSum.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(int arr[], int n, long long sum)
+    {
+        if(sum==0) return {-1};
+        
+        int s=0, e=0;
+        long long csum = 0; //current sum
+        while(e <= n) {
+            while(csum < sum) csum += arr[e++];
+            if(sum == csum) return {s+1,e};
+            if(csum > sum) csum-=arr[s++];       
+        }
+        return {-1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        int arr[n];
+        const int mx = 1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
